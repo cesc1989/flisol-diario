13149a6c0be779f3eb0bd6d3b750a509500e60bb
Francisco Quintero Coronell <frajaquico@yahoo.es>
2024-04-19 22:06:02 -0500

    Form and views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,8 +4,23 @@ class EntriesController < ApplicationController
   end
 
   def index
+    @entries = current_user.entries
   end
 
   def create
+    @entry = Entry.new(entry_params)
+    @entry.user = current_user
+
+    if @entry.save
+      redirect_to entries_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:content)
   end
 end
